1896f48a4f7d69acd7aa2292a72d44c7b8fad171
RaphaelBarcelos <100046113+RaphaelBarcelos@users.noreply.github.com>
2023-03-06 10:10:16 -0300

    mark 2

--- markzuckerberg.php
+++ markzuckerberg.php
@@ -1,25 +1,20 @@
-<body style="background-color: rgb(3, 60, 100);">
+<link rel="stylesheet" href="style.css">
+
+<body style="background-color: rgb(3, 60, 100); color: white;">
     <main>
+    <div class="text">
         <div class="title-mark">
-            <h1>Mark Zuckerberg</h1>
+            <center><h1>Mark Zuckerberg</h1></center>
+            <center><h3>Raphael Barcelos</h3></center>
+            <center><hr></center>
         </div>
         <div class="text-mark">
-            <p>Mark Zuckerberg nasceu em 1982 em Nova York, Estados Unidos, é o cofundador e CEO do Facebook, uma das maiores redes sociais do planeta.</p>
-            <p>Em 2012, o Facebook adquiriu o Instagram por 1 bilhão de dólares e, em 2014, comprou o Whatsapp por 19 bilhões de dólares. </p>
-            <p>Mark Zuckerberg cursou ciências informáticas em Harvard, mas abandonou a universidade em 2004, no seu segundo ano, para se dedicar integralmente ao Facebook. </p>
-            <p>Embora não ter concluído seu curso pela instituição, Zuckerberg recebeu um diploma honorário 12 anos depois de deixar Harvard, quando foi convidado para discursar na cerimônia de formatura da turma de 2016. </p>
-            <p>Durante a universidade, se juntou a um grupo de amigos para desenvolver sites inovadoras, nessa época, baseado em páginas de informações pessoais, Zuckerberg seus colegas criaram o Facemash, um site que publicava fotos de mulheres estudantes e permitia que outros votassem no que consideravam a mais bonita, o que gerou bastante polemica na época. </p>
-            <p>Teve sua ascensão em 2008, quando Zuckerberg ingressou na lista da Forbes, tinha 24 anos e se tornou o americano mais jovem a compor o panteão dos mais ricos da lista, transformou o Facebook em uma das empresas mais bem sucedidas do mundo, recusando todas as ofertas bilionárias que recebeu durante sua trajetória na empresa. </p>
-            <p>Desde criança, Zuckerberg demonstrava interesse por informática. Entre 1998 e 200 estudou na Arley High School, onde se destacou em artes e culturas clássicas. </p>
-            <p>Durante os dois anos seguintes aprendeu Programação Básica com o desenvolvedor de software David Newman e ingressou no Mercy College, onde se graduou em programação. </p>
-            <p>Ao deixa a escola, ingressou na Philips Exeter Academy e de lá partiu para cursos seu ensino superior em Harvard. </p>
-            <p>Em 2001, aos 17 anos, criou um projeto escolar capaz de rastrear preferências de usuários online, chamado "Synapse Media Player" </p>
-            <p>No dia 19 de maio de 2012, Mark Zuckerberg casou-se com sua namorada, a médica pediatra Priscilla Chan, que conheceu em sua universidade, na época ela cursava Biologia. </p>
-            <p>O casal tem 2 filhos, a primeira filha chamada Maxima, nasceu no dia 30 de novembro de 2015 em Los Angeles e a segunda filha nasceu no dia 28 de agosto de 2017, chamado August. </p>
-            <p>Em 2015, o casal doou 45 bilhões de dólares para obras de caridade geridas pela sua própria fundação filantrópica, a "Chan Zuckerberg" </p>
+            <p>Mark Zuckerberg nasceu em 1982 em Nova York, Estados Unidos, é o cofundador e CEO do Facebook, uma das maiores redes sociais do planeta, em 2012, o Facebook adquiriu o Instagram por 1 bilhão de dólares e, em 2014, comprou o Whatsapp por 19 bilhões de dólares, Mark Zuckerberg cursou ciências informáticas em Harvard, mas abandonou a universidade em 2004, no seu segundo ano, para se dedicar integralmente ao Facebook. </p>
+            <p>Embora não ter concluído seu curso pela instituição, Zuckerberg recebeu um diploma honorário 12 anos depois de deixar Harvard, quando foi convidado para discursar na cerimônia de formatura da turma de 2016, durante a universidade, se juntou a um grupo de amigos para desenvolver sites inovadoras, nessa época, baseado em páginas de informações pessoais, Zuckerberg seus colegas criaram o Facemash, um site que publicava fotos de mulheres estudantes e permitia que outros votassem no que consideravam a mais bonita, o que gerou bastante polemica na época, Teve sua ascensão em 2008, quando Zuckerberg ingressou na lista da Forbes, tinha 24 anos e se tornou o americano mais jovem a compor o panteão dos mais ricos da lista, transformou o Facebook em uma das empresas mais bem sucedidas do mundo, recusando todas as ofertas bilionárias que recebeu durante sua trajetória na empresa. </p>
+            <CENTER><img src="./img/mark-zuckerberg.jpg" alt=""></CENTER>
+            <p>Desde criança, Zuckerberg demonstrava interesse por informática. Entre 1998 e 200 estudou na Arley High School, onde se destacou em artes e culturas clássicas, Durante os dois anos seguintes aprendeu Programação Básica com o desenvolvedor de software David Newman e ingressou no Mercy College, onde se graduou em programação, Ao deixa a escola, ingressou na Philips Exeter Academy e de lá partiu para cursos seu ensino superior em Harvard, Em 2001, aos 17 anos, criou um projeto escolar capaz de rastrear preferências de usuários online, chamado "Synapse Media Player" </p>
+            <p>No dia 19 de maio de 2012, Mark Zuckerberg casou-se com sua namorada, a médica pediatra Priscilla Chan, que conheceu em sua universidade, na época ela cursava Biologia, O casal tem 2 filhos, a primeira filha chamada Maxima, nasceu no dia 30 de novembro de 2015 em Los Angeles e a segunda filha nasceu no dia 28 de agosto de 2017, chamado August, Em 2015, o casal doou 45 bilhões de dólares para obras de caridade geridas pela sua própria fundação filantrópica, a "Chan Zuckerberg" </p>
         </div>
-        <div class="image-mark">
-            <img src="" alt="">
         </div>
     </main>
 </body>
